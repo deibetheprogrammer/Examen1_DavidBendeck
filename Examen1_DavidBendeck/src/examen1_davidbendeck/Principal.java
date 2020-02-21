f4ac2c20f65f208ecf98182505afd7bfde290d76
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-02-21 13:55:39 -0600

    Agregué la clase alumnos

--- Examen1_DavidBendeck/src/examen1_davidbendeck/Principal.java
+++ Examen1_DavidBendeck/src/examen1_davidbendeck/Principal.java
@@ -27,17 +27,93 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        TP_MenuPrincipal = new javax.swing.JTabbedPane();
+        P_CrearCuenta = new javax.swing.JPanel();
+        L_Bienvenido = new javax.swing.JLabel();
+        B_CrearTutor = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        P_LogIn = new javax.swing.JPanel();
+        P_CrearExamen = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        L_Bienvenido.setText("Bienvenido");
+
+        B_CrearTutor.setText("Crear Tutor");
+
+        jButton1.setText("Crear Estudiante");
+
+        javax.swing.GroupLayout P_CrearCuentaLayout = new javax.swing.GroupLayout(P_CrearCuenta);
+        P_CrearCuenta.setLayout(P_CrearCuentaLayout);
+        P_CrearCuentaLayout.setHorizontalGroup(
+            P_CrearCuentaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(P_CrearCuentaLayout.createSequentialGroup()
+                .addGroup(P_CrearCuentaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(P_CrearCuentaLayout.createSequentialGroup()
+                        .addGap(263, 263, 263)
+                        .addGroup(P_CrearCuentaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(B_CrearTutor)
+                            .addComponent(L_Bienvenido, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(P_CrearCuentaLayout.createSequentialGroup()
+                        .addGap(250, 250, 250)
+                        .addComponent(jButton1)))
+                .addContainerGap(275, Short.MAX_VALUE))
+        );
+        P_CrearCuentaLayout.setVerticalGroup(
+            P_CrearCuentaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(P_CrearCuentaLayout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addComponent(L_Bienvenido, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(63, 63, 63)
+                .addComponent(B_CrearTutor)
+                .addGap(46, 46, 46)
+                .addComponent(jButton1)
+                .addContainerGap(154, Short.MAX_VALUE))
+        );
+
+        TP_MenuPrincipal.addTab("Crear Cuenta", P_CrearCuenta);
+
+        javax.swing.GroupLayout P_LogInLayout = new javax.swing.GroupLayout(P_LogIn);
+        P_LogIn.setLayout(P_LogInLayout);
+        P_LogInLayout.setHorizontalGroup(
+            P_LogInLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 658, Short.MAX_VALUE)
+        );
+        P_LogInLayout.setVerticalGroup(
+            P_LogInLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 414, Short.MAX_VALUE)
+        );
+
+        TP_MenuPrincipal.addTab("Log In", P_LogIn);
+
+        javax.swing.GroupLayout P_CrearExamenLayout = new javax.swing.GroupLayout(P_CrearExamen);
+        P_CrearExamen.setLayout(P_CrearExamenLayout);
+        P_CrearExamenLayout.setHorizontalGroup(
+            P_CrearExamenLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 658, Short.MAX_VALUE)
+        );
+        P_CrearExamenLayout.setVerticalGroup(
+            P_CrearExamenLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 414, Short.MAX_VALUE)
+        );
+
+        TP_MenuPrincipal.addTab("Crear Examen", P_CrearExamen);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(TP_MenuPrincipal)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(TP_MenuPrincipal)
+                .addContainerGap())
         );
 
         pack();
@@ -79,5 +155,12 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton B_CrearTutor;
+    private javax.swing.JLabel L_Bienvenido;
+    private javax.swing.JPanel P_CrearCuenta;
+    private javax.swing.JPanel P_CrearExamen;
+    private javax.swing.JPanel P_LogIn;
+    private javax.swing.JTabbedPane TP_MenuPrincipal;
+    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
